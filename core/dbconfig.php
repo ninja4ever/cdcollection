a35e449478fb8da3c4ab8fafe598ff45aa56362d
ninja4ever <evilninja@10g.pl>
2015-01-11 18:27:19 +0100

    ssss

--- core/dbconfig.php
+++
@@ -1,5 +0,0 @@
-<?php 
-                    define( "DB_SERVER", "mysql:host=localhost;dbname=cdcollection;port=3306;" );
-                    define( "DB_USERNAME", "root" );
-                    define( "DB_PASSWORD", "");
-?>
